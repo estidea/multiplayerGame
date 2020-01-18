35660771b3e29bf8aaea83c0b6a3c6b2b549a943
estidea <nick.whatsoever@gmail.com>
2020-01-19 00:03:16 +0200

    keyboard input

--- app.js
+++ app.js
@@ -11,33 +11,71 @@ serv.listen(8080);
 console.log("Server is started. Open localhost:8080");
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id) {
+	var self = {
+		x:250,
+		y:250,
+		id:id,
+		name:Math.floor(Math.random()*10),
+		pressingRight:false,
+		pressingLeft:false,
+		pressingTop:false,
+		pressingBottom:false,
+		maxSpeed:10
+	}
+
+	self.updatePosition = function() {
+		if(self.pressingRight)
+			self.x += self.maxSpeed;
+		if(self.pressingLeft)
+			self.x -= self.maxSpeed;
+		if(self.pressingTop)
+			self.y -= self.maxSpeed;
+		if(self.pressingBottom)
+			self.y += self.maxSpeed;
+	}
+	return self;
+}
 
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket) {
 	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
-	socket.name = Math.floor(Math.random()*10);
 	SOCKET_LIST[socket.id] = socket;
+
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
 
 	console.log('New player with id = ' + socket.id + ' has been joined!');
 
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
-		console.log('Player with id = ' + socket.id + ' has been exited.');
-	})
+		delete PLAYER_LIST[socket.id];
+		console.log('Player with id = ' + socket.id + ' has been left.');
+	});
+
+	socket.on('keyPress',function(data){
+		if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		if(data.inputId === 'top')
+			player.pressingTop = data.state;
+		if(data.inputId === 'bottom')
+			player.pressingBottom = data.state;
+	});
 });
 
 setInterval(function(){
 	var pack = [];
-	for(var i  in SOCKET_LIST) {
-		var socket = SOCKET_LIST[i];
-		socket.x++;
-		socket.y++;
+	for(var i  in PLAYER_LIST) {
+		var player = PLAYER_LIST[i];
+		player.updatePosition();
 		pack.push({
-			x:socket.x,
-			y:socket.y,
-			name: socket.name
+			x:player.x,
+			y:player.y,
+			name: player.name
 		});
 	}
 	for(var i  in SOCKET_LIST) {
