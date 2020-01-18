8066a3d680fe345803f990362c1418d1fa3f1939
estidea <nick.whatsoever@gmail.com>
2020-01-18 23:03:12 +0200

    make socket connection

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(8080);
 console.log("Server is started. Open localhost:8080");
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket) {
+	console.log('socket connection');
+
+	socket.on('happy', function(data) {
+		console.log('happy because ' + data.reason);
+	})
+
+	socket.emit('serverMsg', {
+		msg:'accepted'
+	});
+});
