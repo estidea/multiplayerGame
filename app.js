754c166f51c0342e948f7310b27a6d591a1a883d
estidea <nick.whatsoever@gmail.com>
2020-01-18 23:41:09 +0200

    create numbers-chars. Disconnection

--- app.js
+++ app.js
@@ -10,15 +10,39 @@ app.use('/client',express.static(__dirname + '/client'));
 serv.listen(8080);
 console.log("Server is started. Open localhost:8080");
 
+var SOCKET_LIST = {};
+
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket) {
-	console.log('socket connection');
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+	socket.name = Math.floor(Math.random()*10);
+	SOCKET_LIST[socket.id] = socket;
 
-	socket.on('happy', function(data) {
-		console.log('happy because ' + data.reason);
+	console.log('New player with id = ' + socket.id + ' has been joined!');
+
+	socket.on('disconnect',function(){
+		delete SOCKET_LIST[socket.id];
+		console.log('Player with id = ' + socket.id + ' has been exited.');
 	})
-
-	socket.emit('serverMsg', {
-		msg:'accepted'
-	});
 });
+
+setInterval(function(){
+	var pack = [];
+	for(var i  in SOCKET_LIST) {
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y,
+			name: socket.name
+		});
+	}
+	for(var i  in SOCKET_LIST) {
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPositions',pack);
+	}
+
+},20);
